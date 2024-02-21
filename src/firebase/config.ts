e419035f7348057eaec569ed352050ce3e5a5b02
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-21 16:31:19 -0600

    config.ts firebase : variables de entorno agregadas

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,21 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
 const FIRE_BASE_TOKEN = import.meta.env.VITE_API_KEY;
+const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
+const MSG_SENDER_ID = import.meta.env.VITE_MESSAGIND_SENDER_ID;
+const APP_ID = import.meta.env.VITE_APP_ID;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: FIRE_BASE_TOKEN,
-  authDomain: "journal-react-redux-ts.firebaseapp.com",
-  projectId: "journal-react-redux-ts",
-  storageBucket: "journal-react-redux-ts.appspot.com",
-  messagingSenderId: "951243042814",
-  appId: "1:951243042814:web:b6c025206380064c935aaf"
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MSG_SENDER_ID,
+  appId: APP_ID,
 };
 
 // Initialize Firebase
