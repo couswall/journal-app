def39599490ceae67380181a1e7f875d5c391d56
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-21 14:49:11 -0600

    auth thunks: función de checar autenticación añadida

--- src/store/auth/thunks.ts
+++ src/store/auth/thunks.ts
@@ -1,10 +1,29 @@
+import { Action, ThunkDispatch, UnknownAction } from "@reduxjs/toolkit";
+import { checkingCredentials } from ".";
 import { signInWithGoogle } from "../../firebase/providers"
+import { RootState } from "../store";
 
+export type ThunkAction<
+  R, // Return type of the thunk function
+  S, // state type used by getState
+  E, // any "extra argument" injected into the thunk
+  A extends Action // known types of actions that can be dispatched
+> = (dispatch: ThunkDispatch<S, E, A>, getState: () => S, extraArgument: E) => R
+
+//Checking Authentication
+export const checkingAuthentication = ( email: string, password: string ): ThunkAction<void, RootState, unknown, UnknownAction>  => {
+    return async( dispatch ) => {
+
+        dispatch( checkingCredentials() );
+    }
+}
 
 
 //Start SignIn with Google
 export const startGoogleSignIn = () =>{
-    return async( dispatch ){
+    return async( dispatch ) => {
+
+        dispatch( checkingCredentials() );
 
         const result = signInWithGoogle();
     }
