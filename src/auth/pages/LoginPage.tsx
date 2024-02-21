d708e6c44959c61de21b9aaa1ccd631e6bef960f
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-20 21:05:55 -0600

    LoginPage.tsx: Cascaron de formulario creado

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,8 +1,31 @@
+import { Link } from "react-router-dom"
 
 export const LoginPage = () => {
   return (
     <>
-      <h1>LoginPage</h1>
+      <div className="container vh-100 d-flex justify-content-center align-items-center">
+        <form className="p-5 rounded" style={{backgroundColor: 'var(--secondary-color)'}}>
+          <div className="mb-3 ">
+            <label className="form-label text-dark fw-bold">Email address</label>
+            <input type="email" className="form-control"aria-describedby="emailHelp"/>
+            {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+          </div>
+          <div className="mb-3">
+            <label className="form-label text-dark fw-bold">Password</label>
+            <input type="password" className="form-control" />
+          </div>
+          <div className="mb-3 d-flex justify-content-between">
+            <label className="form-label text-dark" >¿No tienes cuenta?</label>
+            <label className="form-label" >
+              <Link to={'/auth/register'} className="text-dark">Registrate</Link>
+            </label>
+          </div>
+          <div className="d-flex justify-content-between">
+            <button type="submit" className="btn btn-primary">Login</button>
+            <button type="submit" className="btn btn-primary">Google</button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
