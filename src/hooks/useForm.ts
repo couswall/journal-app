02bba28e7d29c92f4c2a269a5ec8607996f502c2
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-23 00:33:01 -0600

    useForm.ts: createValidators function completada

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,7 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { Validations } from "../auth/pages";
 
-export const useForm = <T extends Object> ( initialForm: T ) => {
+
+interface formCheckedValidation {
+    [key: string ]: null | string; 
+}
+
+export const useForm = <T extends Record<string,string>> ( initialForm: T, formValidations: Validations = {} ) => {
+    
     const [formState, setFormState] = useState<T>( initialForm );
+    const [ formValidation, setFormValidation ] = useState<formCheckedValidation>({});
 
     const onInputChange = ( {target}: React.ChangeEvent<HTMLInputElement> ):void => {
         
@@ -16,11 +24,31 @@ export const useForm = <T extends Object> ( initialForm: T ) => {
         setFormState( initialForm );
     }
 
+    useEffect( () => {
+        createValidators();
+    }, [formState]);
+
+
+    //Validación de Formulario
+    const createValidators = () => {
+        
+        const formCheckedValues: formCheckedValidation = {}; 
+
+        for (const formField of Object.keys( formValidations )) {
+            
+            const [ fn, errorMessage = 'Este campo es requerido'] = formValidations[ formField ];
+
+            formCheckedValues[`${ formField }Valid` ] = fn( formState[formField] ) ? null : errorMessage;
+
+            setFormValidation( formCheckedValues );
+        }
+    }
 
     return{
         ...formState,
         formState, 
         onInputChange, 
-        onResetForm
+        onResetForm,
+        ...formValidation,
     }
 }
