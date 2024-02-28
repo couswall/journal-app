23b58044cfdbb8fa5a9d70bae3edb571d16dbba4
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-27 19:10:49 -0600

    JournalLayout.tsx creado

---
+++ src/journal/layout/JournalLayout.tsx
@@ -0,0 +1,10 @@
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <div className="container-fluid vh-100 p-0" style={{backgroundColor: 'var(--light-color)'}}>
+
+    
+    { children }
+    </div>
+  )
+}
