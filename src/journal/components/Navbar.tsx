6b0aa2319373d9ffc6ed8ce3c73948a5dceddb81
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-27 20:12:43 -0600

    Navbar.tsx: Función de logout definida

--- src/journal/components/Navbar.tsx
+++ src/journal/components/Navbar.tsx
@@ -3,14 +3,22 @@ import { IoMdExit } from "react-icons/io"
 import './styles';
 
 export const Navbar = () => {
+
+  const onLogout = () => {
+    console.log('logout');
+  }
+
   return (
     <nav className="nav">
-        <div className="container-fluid d-flex justify-content-between align-items-center mx-3">
+      <div className="container-fluid d-flex justify-content-between align-items-center mx-3">
         <h1 className="fs-5">JournalApp</h1>
-        <button className="btn text-white fs-5">
+        <button 
+          className="btn text-white fs-5"
+          onClick={ () => onLogout() }
+        >
             <IoMdExit />
         </button>
-        </div>
+      </div>
     </nav>
   )
 }
