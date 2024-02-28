5b851670fd6123a8fb968fc9bdf19c4d6e23f56a
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-27 19:21:23 -0600

    Navbar.tsx terminado

--- src/journal/components/Navbar.tsx
+++ src/journal/components/Navbar.tsx
@@ -1,6 +1,16 @@
+import { IoMdExit } from "react-icons/io"
+
+import './styles';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="nav">
+        <div className="container-fluid d-flex justify-content-between align-items-center mx-3">
+        <h1 className="fs-5">JournalApp</h1>
+        <button className="btn text-white fs-5">
+            <IoMdExit />
+        </button>
+        </div>
+    </nav>
   )
 }
