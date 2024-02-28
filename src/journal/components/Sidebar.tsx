f179000fb6521bb70162edec20dc8c2a4d0a28e2
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-27 21:34:15 -0600

    Sidebar.tsx: displayName agregado

--- src/journal/components/Sidebar.tsx
+++ src/journal/components/Sidebar.tsx
@@ -1,15 +1,19 @@
+import { useSelector } from 'react-redux';
 import { SidebarItem } from '.';
 import './styles';
+import { RootState } from '../../store';
 
 
 export const Sidebar = () => {
+
+    const { displayName } = useSelector( (state: RootState ) => state.auth );
   
     const months = ["Enero", "Febrero", "Marzo", "Abril"];
   
     return (
     <div className="sidebar d-flex flex-column align-items-stretch flex-shrink-0 vh-100 shadow-sm">
         <div className="name-container d-flex align-items-center justify-content-center w-100 shadow-sm">
-            <h1 className="fs-5 text-primary-color">Andre Ignorosa</h1>
+            <h1 className="fs-5 text-primary-color">{ displayName }</h1>
         </div>
 
         <ul className="list-group list-group-flush border-bottom scrollarea ">
