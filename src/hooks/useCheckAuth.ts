9f7d6949c4c79c48103d4cee0c101c9738b9f4e4
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-29 13:37:50 -0600

    useCheckAuth.ts: dispatch de startLoadingNotes agregado

--- src/hooks/useCheckAuth.ts
+++ src/hooks/useCheckAuth.ts
@@ -4,6 +4,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { FirebaseAuth } from "../firebase/config";
 import { login, logout } from "../store/auth";
 import { useEffect } from "react";
+import { startLoadingNotes } from "../store/journal";
 
 
 
@@ -22,6 +23,7 @@ export const useCheckAuth = () => {
         const { uid, displayName, email, photoURL } = user;
         
         dispatch( login({ uid, displayName, email, photoURL, }) );
+        dispatch( startLoadingNotes() );
       })
       
     }, []);
