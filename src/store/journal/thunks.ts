5bd06486519289facdfb4cca5e285ef9d2c69ceb
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-29 12:23:28 -0600

    journal thunks.ts: Función startNewNote definida

--- src/store/journal/thunks.ts
+++ src/store/journal/thunks.ts
@@ -1,5 +1,41 @@
+import { UnknownAction } from "@reduxjs/toolkit";
+import { ThunkAction } from "../auth";
+import { RootState } from "../store";
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../firebase/config";
+import { addNewEmptyNote, isSavingNote, setActiveNote } from ".";
+
+interface newAddedNote {
+    id?: string; 
+    title: string; 
+    body: string; 
+    imageUrls: string[];
+    date: number; 
+}
 
 // Función para agregar una nueva nota
-export const startNewNote = () => {
+export const startNewNote = (): ThunkAction<void, RootState, unknown, UnknownAction> => {
+    return async( dispatch, getState ) => {
+
+        dispatch( isSavingNote() );
+
+        const { uid } = getState().auth;
+
+        const newNote: newAddedNote = {
+            title: '', 
+            body: '', 
+            imageUrls: [],
+            date: new Date().getTime()
+        }
+
+        const newDoc = doc( collection( FirebaseDB, `${uid}/journal/notes` ) );
+        await setDoc( newDoc, newNote );
+        
+        newNote.id = newDoc.id
+
+        dispatch( addNewEmptyNote( newNote ) );
+        dispatch( setActiveNote( newNote ) )
+    }
+
 
 }
