190b193df624ded8c092ba40c0f7f2017c3f8316
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-29 20:54:18 -0600

    Sidebar.tsx: props de la nota agregadas

--- src/journal/components/SidebarItem.tsx
+++ src/journal/components/SidebarItem.tsx
@@ -1,15 +1,21 @@
 import { MdOutlineTurnedInNot } from "react-icons/md"
 import './styles';
+import { NoteState } from "../../store/journal";
 
-export const SidebarItem = ({item}) => {
+
+interface SidebarItemProps{
+  note: NoteState
+}
+
+export const SidebarItem = ({ note }: SidebarItemProps) => {
   return (
     <li className="sidebar-item list-group-item d-flex py-4 border-bottom align-items-center gap-4">
         <div className="fs-4">
             <MdOutlineTurnedInNot />
         </div>
         <div>
-            <h6 className="mb-2 d-inline-block d-flex flex-column">{ item }</h6>
-            <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, sunt!</small>
+            <h6 className="mb-2 d-inline-block d-flex flex-column">{ note.title }</h6>
+            <small>{ note.body }</small>
         </div>
     </li>
   )
