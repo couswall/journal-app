d9147ee2d20d8800da5045eb8ae7fe2a346b414b
Andre Ignorosa <andreiglu@hotmail.com>
2024-02-29 20:53:04 -0600

    Sidebar.tsx: Notas tomadas del store

--- src/journal/components/Sidebar.tsx
+++ src/journal/components/Sidebar.tsx
@@ -7,19 +7,18 @@ import { RootState } from '../../store';
 export const Sidebar = () => {
 
     const { displayName } = useSelector( (state: RootState ) => state.auth );
-  
-    const months = ["Enero", "Febrero", "Marzo", "Abril"];
-  
+    const { notes = [] } = useSelector( (state: RootState ) => state.journal );
+    
     return (
-    <div className="sidebar d-flex flex-column align-items-stretch flex-shrink-0 vh-100 shadow-sm">
+    <div className="sidebar d-flex flex-column align-items-stretch flex-shrink-0 shadow-sm">
         <div className="name-container d-flex align-items-center justify-content-center w-100 shadow-sm">
             <h1 className="fs-5 text-primary-color">{ displayName }</h1>
         </div>
 
-        <ul className="list-group list-group-flush border-bottom scrollarea ">
+        <ul className="list-group list-group-flush border-bottom"> 
         {
-            months.map( (month, index) => (
-                <SidebarItem key={ index } item={ month }/>
+            notes.map( (note) => (
+                <SidebarItem key={ note.id } note = { note }/>
             ))
         }
         </ul>
