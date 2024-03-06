571b6c6bb5e8e59583bb934a82b73b80a7a6e794
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-05 18:04:01 -0600

    ImageGallery.tsx e ImageGallery.css creados

---
+++ src/journal/components/ImageGallery.tsx
@@ -0,0 +1,12 @@
+
+interface ImageGalleryProps{
+    imagesArray: string[]
+}
+
+export const ImageGallery = ( {imagesArray = []}: ImageGalleryProps ) => {
+  return (
+    <div className="container-fluid mb-2" >
+       
+    </div>
+  )
+}
--- src/journal/components/styles/index.ts
+++ src/journal/components/styles/index.ts
@@ -1,3 +1,4 @@
 export * from './Navbar.css'
 export * from './Sidebar.css'
 export * from './SidebarItem.css'
+export * from './ImageGallery.css'
