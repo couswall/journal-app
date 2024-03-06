f4b597532f95a537d7d0f6a06989b6471d1e3503
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-05 18:27:27 -0600

    journalSlice.ts: Reducer setPhotosToActiveNote definido

--- src/store/journal/journalSlice.ts
+++ src/store/journal/journalSlice.ts
@@ -72,10 +72,18 @@ export const journalSlice = createSlice({
     deleteNoteById: ( state, action ) => {
       state.active = null; 
       state.notes = state.notes.filter( note => note.id !== action.payload );
-    }
+    },
+
+    //Añade las fotos a la nota activa
+    setPhotosToActiveNote: ( state, action ) => {
+      state.active!.imageUrls= [...state.active!.imageUrls, ...action.payload]; 
+      state.isSaving = false; 
+    }, 
+
+
    
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { addNewEmptyNote, setActiveNote, setNotes, setSaving, updateNote, deleteNoteById, isSavingNote } = journalSlice.actions
+export const { addNewEmptyNote, setActiveNote, setNotes, setSaving, updateNote, deleteNoteById, isSavingNote, setPhotosToActiveNote } = journalSlice.actions
