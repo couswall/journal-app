147415019bb0cd98df50c0ea8035ebd0039b7d22
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-05 18:22:57 -0600

    ImageGallery.tsx: estructura de galería definida

--- src/journal/components/ImageGallery.tsx
+++ src/journal/components/ImageGallery.tsx
@@ -1,4 +1,6 @@
 
+import './styles'
+
 interface ImageGalleryProps{
     imagesArray: string[]
 }
@@ -6,7 +8,19 @@ interface ImageGalleryProps{
 export const ImageGallery = ( {imagesArray = []}: ImageGalleryProps ) => {
   return (
     <div className="container-fluid mb-2" >
-       
+        <div className="gallery-wrapper row">
+            {
+                imagesArray.map( image => (
+                    <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2 image-gallery" key={ image }>
+                        <img src={image} 
+                            alt={ image } 
+                            className="img-thumbnail" 
+                            loading="lazy"
+                        />
+                    </div>
+                ))
+            }
+        </div>
     </div>
   )
 }
