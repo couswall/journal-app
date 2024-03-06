b7aaf0d6e5e6c89eb0f44c9c9d06295b8fd3a435
Andre Ignorosa <andreiglu@hotmail.com>
2024-03-05 18:26:32 -0600

    fileUpload.ts: Función fileUpload definida

--- src/helpers/fileUpload.ts
+++ src/helpers/fileUpload.ts
@@ -1,4 +1,32 @@
 
-export const fileUpload = () => {
-  
+const cloudName = import.meta.env.VITE_CLOUDINARY_NAME; 
+const cloudFolder = import.meta.env.VITE_CLOUDINARY_FOLDER; 
+
+export const fileUpload = async ( file: File ) => {
+    
+    if( !file ) throw new Error("No hay ningún archivo para subir");
+
+    const cloudUrl = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+    
+    const formData = new FormData(); 
+    formData.append("upload_preset", cloudFolder); 
+    formData.append("file", file); 
+    
+    try {
+
+        const resp = await fetch( cloudUrl, {
+            method: 'POST',
+            body: formData
+        }); 
+
+        const cloudResp = await resp.json(); 
+        // console.log( cloudResp ); 
+
+        return cloudResp.secure_url; 
+        
+    } catch (error) {
+        // console.log( error );
+        if ( error instanceof Error) throw new Error( error.message ); 
+        
+    }
 }
